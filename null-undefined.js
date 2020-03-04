9bc3d848ee29500c29f6ee57e57f9d43e5040cb3
Roksana Akter <roksanalive@gmail.com>
2020-03-04 09:36:40 +0600

    practice

---
+++ null-undefined.js
@@ -0,0 +1,38 @@
+let bird;
+console.log(bird);
+
+///////////////////
+
+function add (num1, num2){
+    console.log(num1 + num2);
+    return 
+}
+
+const result = add (15, 20);
+console.log(result);
+
+/////////////////////////
+
+function add (num1, num2){
+    console.log(num1, num2);
+     
+}
+
+const result = add (15);
+console.log(result);
+
+///////////////////////
+
+const man1 = {name:"samir", id:"15896"};
+console.log(man1.phone);
+
+///////////////////////
+
+let ages = [12, 25, 35]
+console.log(ages[9]);
+
+/////////////////////////
+
+let fun = undefined;
+console.log(fun);
+
